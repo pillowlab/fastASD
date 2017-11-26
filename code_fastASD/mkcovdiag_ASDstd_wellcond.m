81e4b2e4ed988a0e6776c727182799343d529684
Jonathan Pillow <jpillow@princeton.edu>
2017-11-26 04:52:59 -0500

    added code to implement a "well-conditioned" version that adds a small diagonal matrix instead of truncating small eigenvalues

--- code_fastASD/mkcovdiag_ASDstd_wellcond.m
+++ code_fastASD/mkcovdiag_ASDstd_wellcond.m
@@ -1,4 +1,4 @@
-function cdiag = mkcovdiag_ASDstd_wellcond(len,trho,wwnrm,adddiag)
+function [cdiag,dcinvdthet,dcdthet,ddcinvdthet] = mkcovdiag_ASDstd_wellcond(len,trho,wwnrm,adddiag)
 % Eigenvalues of ASD covariance (as diagonalized in Fourier domain),
 % well-conditioned by adding a constant offset
 %
@@ -12,14 +12,39 @@ function cdiag = mkcovdiag_ASDstd_wellcond(len,trho,wwnrm,adddiag)
 %        trho [1 x 1] or [n x 1] - Fourier domain prior variance
 %       wwnrm [n x 1] - vector of squared normalized Fourier frequencies
 %     adddiag [1 x 1] - additive constant to keep eigenspectrum well
-%                       conditioned (DEFAULT = 10e-08)
+%                       conditioned (DEFAULT = 10e-06)
 %        
 % OUTPUT:
 %      cdiag [n x 1] - vector of eigenvalues of C for frequencies in w
 
 if nargin < 4
-    adddiag = 1e-08;
+    adddiag = 1e-6;
 end
 
 % Compute diagonal of ASD covariance matrix
-cdiag = trho.*exp(-.5*wwnrm.*len.^2)+adddiag;
+cdiag0 = trho.*exp(-.5*wwnrm.*len.^2);
+cdiag = cdiag0+adddiag;
+
+% 1st derivative of inv(Cdiag)
+if nargout > 1
+    dcinvdthet = [(len.*wwnrm).*(cdiag0./cdiag.^2), ...  % dC^-1/dl
+        -(1./trho).*(cdiag0./cdiag.^2)];        % dC^-1/drho
+end
+
+% 1st derivative of Cdiag 
+if nargout > 2
+    dcdthet = [-len.*wwnrm.*cdiag0, ...   % dC/dl
+        (cdiag0./trho)]; % dC/drho
+        
+end
+
+% 2nd derivative of inv(Cdiag)
+if nargout > 3
+    ddCdl2 = (-wwnrm+len.^2*wwnrm.^2).*cdiag0; % 2nd deriv of C w.r.t. l
+    ddCdlr = dcdthet(:,1)./trho; % 2nd deriv of C w.r.t. l by rho
+    ddcinvdthet = ...
+        [(-cdiag.*ddCdl2 + 2*dcdthet(:,1).^2)./cdiag.^3, ...% d^2 C^-1 /dl^2
+        (-cdiag.*ddCdlr + 2*dcdthet(:,1).*dcdthet(:,2))./cdiag.^3, ... d^2 C^-1 / dl dr
+        2*dcdthet(:,2).^2./cdiag.^3]; % d^2 C^-1 dr
+
+end
---
+++ demos/test_fastASD_1Dwellconditioned.m
@@ -0,0 +1,75 @@
+% test_fastASD_1Dwellconditioned.m
+%
+% Check code that computes evidence without truncating small eigenvalues
+% (using a "well-conditioned" version of the ASD prior covariance matrix
+% that has a small constant diagonal added to it).
+%
+% Uses simulated example from test_fastASD_1D.m
+
+% NOTE: Run script 'setpaths.m' in parent directory before running
+
+
+%% Generate simulated dataset
+
+% Set prior distribution for filter vector
+nk = 1000; % number of filter coeffs (1D vector)
+rho = 2;   % marginal variance of prior
+len = 50; % length scale of prior
+C0 = mkcov_ASD(len,rho,nk); % prior covariance matrix 
+
+% Sample true filter from the prior distribution
+k_true = mvnrnd(zeros(1,nk),C0)'; % sample ktrue from multivariate normal 
+
+% Generate stimuli and sample responses from model
+nsamps = 500; % number of stimulus samples
+signse = 10;  % stdev of added noise
+x = gsmooth(randn(nk,nsamps),1)'; % make smooth stimuli 
+y = x*k_true + randn(nsamps,1)*signse; % simulate neural response
+
+
+%% Compute ASD estimate 
+fprintf('\n\n...Running ASD...\n');
+
+% Set lower bound on length scale.  (Larger -> faster inference).
+% If it's set too high, the function will warn that optimal length scale is
+% at this lower bound, and you should re-run with a smaller value since
+% this may be too high
+minlen = 20; % minimum length scale
+
+% Run ASD and return sufficient statistics 
+[kasd,asdstats,dd] = fastASD(x,y,nk,minlen);
+[kasd_wc1,asdstats_wc1,dd_wc1] = fastASD_wellcond(x,y,nk,minlen,[],1e-7);
+[kasd_wc2,asdstats_wc2,dd_wc2] = fastASD_wellcond(x,y,nk,minlen,[],1e-3);
+
+
+%% Plot + Report Results
+
+subplot(221);
+h = plot(1:nk, k_true, 'k', 1:nk, kasd, 1:nk, kasd_wc1, '--', 1:nk, kasd_wc2, '-.');
+set(h(2:3),'linewidth', 2);
+legend('true k', 'asd estimate', 'well-cond asd1', 'well-cond asd2');
+title('MAP estimates');
+
+subplot(222);
+h = plot(1:nk, kasd-kasd, 1:nk, kasd-kasd_wc1, '--', 1:nk, kasd-kasd_wc2, '-.');
+set(h(1:2),'linewidth', 2);
+legend('asd estimate', 'well-cond asd1', 'well-cond asd2');
+title('MAP estimate errors');
+
+subplot(212);
+nf = length(dd.wwnrm); % number of fourier coeffs
+cdiag1 = zeros(nf,1); 
+cdiag2 = zeros(nf,1);
+cdiag3 = zeros(nf,1);
+cdiag1(dd.ii) = 1./diag(dd.Cinv);
+cdiag2(dd_wc1.ii) = 1./diag(dd_wc1.Cinv);
+cdiag3(dd_wc2.ii) = 1./diag(dd_wc2.Cinv);
+semilogy(1:nfourier,cdiag1, 'o-', 1:nfourier, cdiag2, '--', 1:nf,cdiag3, '-.','linewidth', 2)
+axis tight;
+title('Cdiag entries of learned prior covariance');
+legend('asd', 'well-cond asd1', 'well-cond asd2');
+
+fprintf('\nEvidence values:\n=================\n');
+fprintf('Standard ASD: %.2f\n', -asdstats.neglogEv);
+fprintf('Wellcond ASD1: %.2f\n', -asdstats_wc1.neglogEv);
+fprintf('Wellcond ASD2: %.2f\n', -asdstats_wc2.neglogEv);
